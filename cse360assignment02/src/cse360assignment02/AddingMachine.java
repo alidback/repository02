70617db8133b176c15e92b6a1bd8f8cfb367482b
alidback <alidback@asu.edu>
2020-10-07 22:57:19 -0500

    New version of AddingMachine file

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,31 @@ package cse360assignment02;
 
 public class AddingMachine { //Adding Machine class
   private int total;
+  private String str = "0";
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
   public int getTotal () { //returns total
-    return 0;
+    return total;
   }
   
   public void add (int value) { //adds values
+	  total = total + value;
+	  str = str + " + " + value;
   }
 
   public void subtract (int value) { //subtracts values
+	  total = total - value;
+	  str = str + " - " + value;
   }
 
   public String toString () { //returns string
-    return "";
+    return str;
   }
 
   public void clear() { //clears things
+	  str = "0";
   }
 }
