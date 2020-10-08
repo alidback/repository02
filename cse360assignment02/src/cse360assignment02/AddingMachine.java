65297adef35a0967bf84bf6fb474d87b8b8d0664
Andrew L <Andrew L@10.0.0.22>
2020-10-07 22:02:32 -0500

    Initial commit

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine { //Adding Machine class
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () { //returns total
+    return 0;
+  }
+  
+  public void add (int value) { //adds values
+  }
+
+  public void subtract (int value) { //subtracts values
+  }
+
+  public String toString () { //returns string
+    return "";
+  }
+
+  public void clear() { //clears things
+  }
+}
